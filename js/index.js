f4aabbf4cd98ae884023ef7925befc2dca3ed6fb
Chris Ryan <creditdesign@gmail.com>
2013-08-31 09:19:04 +0100

    Adds svg overlay for tooltips

--- js/index.js
+++ js/index.js
@@ -9,16 +9,27 @@ var allValues = [];
 var displayYear = "value2012";
 
 
-/* set up canvas, get tallest column height */
-// var canvas = document.getElementById('canvas-map');
-// var ctx = canvas.getContext('2d');	
-
-
-var canvas = d3.select(".count-map").append("canvas")
+/* set up canvas */
+var canvas = d3.select(".canvas-map").append("canvas")
     .attr("width", width)
     .attr("height", height);
 
 var context = canvas.node().getContext("2d");
+
+
+/*	Create SVG element */
+var holder = d3.select(".svg-map")
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+
+/* 	Add a group to hold the map and circles */
+var svg = holder.append("g")
+		.attr("transform", "translate(0,0)")
+		.attr("transform", "scale(1)");
+
+
+
 
 
 d3.csv('data/gpcp_anomalies_1979-2012-edit-2.csv', function(d) {
@@ -86,7 +97,7 @@ d3.csv('data/gpcp_anomalies_1979-2012-edit-2.csv', function(d) {
 
 
 function draw() {
-
+	
 	/* Event listner for when the user changes the adjust scale checkbox */
 	d3.selectAll("select#selectYear").on("change", function() {
 		console.log(this.value);
@@ -104,8 +115,6 @@ function draw() {
 		displayYear = this.value;
 		updateYear();
 	};
-
-
 
 	var maxLong = d3.max(dataset, function (d) {
 		return d.long;
@@ -137,8 +146,6 @@ function draw() {
 	var rectWidth = Math.round(width / (maxLong - minLong)) + 1;
 	var rectHeight = Math.round(height / -(minLat - maxLat)) + 1;
 
-
-
 	/*	Define colour scale */
 	var colourScale = d3.scale.linear().domain([-3, -2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3]).range(["#442B0C", "#5A3E17", "#8F6823", "#C3AE4A", "#96B247", "#53B264", "#2CB1A0", "#3EC6DC", "#348FCA", "#2E52A5", "#31328C", "#1F1B5A", "#1A2051"]);
 
@@ -168,7 +175,6 @@ function draw() {
 				var posX = xScale(dataset[i].long);
 				var posY = yScale(dataset[i].lat);
 
-
 				context.fillStyle = colourScale(dataset[i]["" + displayYear + ""]);
 				context.fillRect( posX, posY, rectWidth, rectHeight);
 
@@ -176,14 +182,98 @@ function draw() {
 
 		}
 
-	createSlider();
-	updateYear();
 
 
 
+	function createCanvas () {
+
+
+		for (var i = 0; i < dataset.length; i++) {
+			var posX = xScale(dataset[i].long);
+			var posY = yScale(dataset[i].lat);
+
+			context.fillStyle = colourScale(dataset[i]["" + displayYear + ""]);
+			context.fillRect( posX, posY, rectWidth, rectHeight);
+			
+			if (i === (dataset.length - 1)) {
+		
+			}
+		};
+	}
+
+
+	function createSvg () {
+		
+		var rects = svg.selectAll("rect")
+				.data(dataset)
+				.enter()
+				.append("rect")
+				.style("opacity", 0)
+				.attr("x", function(d) {
+							return xScale(d.long);
+						})
+				.attr("y", function(d) {
+							return yScale(d.lat);
+						})
+				.attr("width", rectWidth)
+				.attr("height", rectHeight)
+				.each(function(d,i) { 
+					if (i === (dataset.length - 1 ) ) {
+						d3.select(".outer-wrapper .count-map img").style("display","none");
+						console.log("svg finished!");
+					}
+				});
+
+			rects.on("mouseover", function (d,i) {
+
+
+					var x = d3.select(this).attr("x");
+					var y = d3.select(this).attr("y");
+
+					/* Get this rects's x/y values, then augment for the tooltip */
+					var xPosition = parseInt(x) - (parseInt($(".tooltip").css("width"))/2) -10;
+					var yPosition = parseInt(y) - (parseInt($(".tooltip").css("height")) * 2) -8 ;
+
+					/* Update the tooltip position and value */
+					d3.select(".tooltip")
+						.style("left", xPosition + "px")
+						.style("top", yPosition + "px");
+
+					/* Update the tooltip text */
+					d3.select(".tooltip")
+						.select(".value")
+						.html(d["" + displayYear + ""]);
+
+
+					/* Show the tooltip */
+					d3.select(".tooltip")
+						.classed("hidden", false)
+						.transition()
+						.duration(duration/2)
+						.style("opacity", 1);
+
+				}).on("mouseout", function(){
+					d3.select(".tooltip")
+						.transition()
+						.duration(duration/2)
+						.style("opacity", 0)
+						.each("end", function() {
+							d3.select(".tooltip").classed("hidden", true);
+						});
+				})
+		}
+
+		createCanvas();
+		createSlider();
+
+		window.setTimeout(function() {
+			console.log("building that svg now");
+				createSvg();
+		}, 500);
 
 
 }
+
 
 
 
