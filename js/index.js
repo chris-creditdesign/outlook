2c73007def472e0c1b2bf96bbb02aab4f8b6ba75
Chris Ryan <creditdesign@gmail.com>
2013-09-14 15:01:00 +0100

    Adds location pointers

--- js/index.js
+++ js/index.js
@@ -118,7 +118,7 @@ function draw() {
 	$(".status-message").css({"display":"none"});
 
 
-	/*	Create the SVG and its elements */
+	/*	Add SVG elements to the play and pause buttons */
 	var playSvg = d3.select("button.play")
 					.append("svg")
 					.attr("width", 50 )
@@ -127,6 +127,7 @@ function draw() {
 					.attr("d", "M11.166,23.963L22.359,17.5c1.43-0.824,1.43-2.175,0-3L11.166,8.037c-1.429-0.826-2.598-0.15-2.598,1.5v12.926C8.568,24.113,9.737,24.789,11.166,23.963z")
 					.style("pointer-events","none")
 					.style("fill", "#ffffff")
+					.style("stoke", "none")
 					.attr("transform", "translate(5,2), scale(1.25)");
 
 
@@ -142,6 +143,7 @@ function draw() {
 		.attr("y", 10)
 		.style("pointer-events","none")
 		.style("fill", "#ffffff")
+		.style("stoke", "none");
 
 
 	pauseSvg.append("rect")
@@ -151,6 +153,19 @@ function draw() {
 		.attr("y", 10)
 		.style("pointer-events","none")
 		.style("fill", "#ffffff")
+		.style("stoke", "none");
+
+	/*	Add SVG elements to the pointers */
+	var pointerSvg = d3.selectAll(".outer-wrapper .count-map span.pointer")
+					.append("svg")
+					.attr("width", 45 )
+					.attr("height", 45 )
+				  .append("path")
+					.attr("d", "M26.711,14.086L16.914,4.29c-0.778-0.778-2.051-0.778-2.829,0L4.29,14.086c-0.778,0.778-0.778,2.05,0,2.829l9.796,9.796c0.778,0.777,2.051,0.777,2.829,0l9.797-9.797C27.488,16.136,27.488,14.864,26.711,14.086zM14.702,8.981c0.22-0.238,0.501-0.357,0.844-0.357s0.624,0.118,0.844,0.353c0.221,0.235,0.33,0.531,0.33,0.885c0,0.306-0.101,1.333-0.303,3.082c-0.201,1.749-0.379,3.439-0.531,5.072H15.17c-0.135-1.633-0.301-3.323-0.5-5.072c-0.198-1.749-0.298-2.776-0.298-3.082C14.372,9.513,14.482,9.22,14.702,8.981zM16.431,21.799c-0.247,0.241-0.542,0.362-0.885,0.362s-0.638-0.121-0.885-0.362c-0.248-0.241-0.372-0.533-0.372-0.876s0.124-0.638,0.372-0.885c0.247-0.248,0.542-0.372,0.885-0.372s0.638,0.124,0.885,0.372c0.248,0.247,0.372,0.542,0.372,0.885S16.679,21.558,16.431,21.799z")
+					.style("pointer-events","none")
+					.style("fill", "#666666")
+					.style("stoke", "none")
+					.attr("transform", "translate(-4,-4), scale(1.7)");
 
 
 
